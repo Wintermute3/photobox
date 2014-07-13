2b86e34e523b04d57658e24153ff577d97d61bd6
Michael Nagy <mnagy@voalte.com>
2014-07-12 22:22:51 -0500

    Sort of working with images.  Canvas will not render...

--- photobox.py
+++ photobox.py
@@ -8,7 +8,8 @@ CONTACT = 'bright.tiger@gmail.com'
 # Graphical photo management layer on top of a neo4j database.
 # -------------------------------------------------------------------------------
 
-import os, sys, tempfile, subprocess, time, datetime, pprint, glob
+import os, sys, math
+import tempfile, subprocess, time, datetime, pprint, glob
 
 def CrashAndBurn(library):
   print()
@@ -20,6 +21,16 @@ try:
   from py2neo import neo4j, node, rel
 except:
   CrashAndBurn('py2neo')
+
+try:
+  from tkinter import *
+except:
+  CrashAndBurn('tkinter')
+
+try:
+  from PIL import Image, ImageTk
+except:
+  CrashAndBurn('PIL.Image/ImageTk')
 
 # -------------------------------------------------------------------------------
 # Do a command and return the result as a string.  If an error occurs, prefix
@@ -87,20 +98,22 @@ def Neo4j_Init():
   return Running
 
 # -------------------------------------------------------------------------------
-# Our schema is built out on the following node types:
+# Our schema is built out of the following node types:
 #
 #   pix . . . . filename, filesize, color/monochrome flag, image dimensions and tags:
 #                 human-readable/searchable keywords
 #                 human-readable freeform text
 #                 machine-readable/searchable keywords
-#   set . . . . name, ordered group of photos
 #
+#   set . . . . name, ordered group of photos
 # -------------------------------------------------------------------------------
+
+db = None
 
 SetIndex = None
 
 def AddSetToSet(Name, Parent):
-  global SetIndex
+  global SetIndex, db
   Set, = db.create({'name': Name})
   Set.add_labels('Set')
   SetIndex.add('name', Name, Set)
@@ -116,11 +129,9 @@ def GetSetByName(Name):
   Set, = SetIndex.get('name', Name)
   return Set
 
-from PIL import Image
-
 PixIndex = None
 
-def NewPixList(Filter):
+def AddPixListFromFilePathMask(Filter):
   global PixIndex
   Filenames = glob.glob(Filter)
   Pixs = []
@@ -130,79 +141,150 @@ def NewPixList(Filter):
     Pix.add_labels('Pix')
     PixIndex.add('filename', Filename, Pix)
     print('>>> Added Pix %s: [%s]' % (Pix._id, Pix['filename']))
-    #x = Image.open(Filename)
-    #x.thumbnail((128,128))
-    #x.show()
   return Pixs
 
+def ShowPix(Pix):
+  Image.open(Pix['filename']).show()
+
+def ShowPixList(PixList):
+  for Pix in PixList:
+    ShowPix(Pix)
+
+def ShowPixListThumbnails(PixList):
+  for Pix in PixList:
+    x = Image.open(Pix['filename'])
+    x.thumbnail((128,128))
+    x.show()
+
 def AddPixListToSet(PixList, Set):
+  global db
   for Pix in PixList:
     db.create(rel(Pix, 'IN', Set))
 
-def GetPixListByFilename(Filename):
-  return PixIndex.get('filename', Filename)
+def GetPixByFilename(Filename):
+  try:
+    return PixIndex.get('filename', Filename)[0]
+  except:
+    return None
 
 def GetRelListOfPix(Pix):
   return Pix.match_outgoing(rel_type='IN')
-  #return list(graph_db.match(start_node=Pix, rel_type='IN'))
 
 def GetSetByNames(Pix):
+  global db
   return list(graph_db.match(start_node=Pix, rel_type='IN'))
 
+def Neo4j_Test():
+  global db, SetIndex, PixIndex
+  if Neo4j_Init():
+    db = neo4j.GraphDatabaseService('http://localhost:7474/db/data/')
+    db.clear() # always start with an empty database until we are debugged
 
-#  neo4j.Path(Pix, 'IN', )
+    SetIndex = db.get_or_create_index(neo4j.Node, 'Set')
+    PixIndex = db.get_or_create_index(neo4j.Node, 'Pix')
 
-#import matplotlib.pyplot as plt
+    Families = NewSet('Families')
 
-#def ShowPhotos(Set):
-#  x=plt.imread(Filename)
-#  plt.imshow(x)
-#  plt.show()
+    AddSetToSet( 'Nagy'      , Families)
+    AddSetToSet( 'Tyson'     , Families)
+    AddSetToSet( 'Hedgepeth' , Families)
+    AddSetToSet( 'Strickland', Families)
+    AddSetToSet( 'Kosik'     , Families)
+    AddSetToSet( 'Marmaro'   , Families)
+    AddSetToSet( 'Caps'      , Families)
+    AddSetToSet( 'Rankin'    , Families)
 
-if Neo4j_Init():
-  db = neo4j.GraphDatabaseService('http://localhost:7474/db/data/')
-  db.clear() # always start with an empty database.
+    Friends = NewSet('Friends')
+    Circus = AddSetToSet('Circus', Friends)
 
-  SetIndex = db.get_or_create_index(neo4j.Node, 'Set')
-  PixIndex = db.get_or_create_index(neo4j.Node, 'Pix')
+    PixList = AddPixListFromFilePathMask('images/*.jpg')
+    if len(PixList) == 0:
+      print('*** No jpg files found in working directory!')
+      os._exit(1)
 
-  Families = NewSet('Families')
+    AddPixListToSet(PixList, Circus)
 
-  AddSetToSet( 'Nagy'      , Families)
-  AddSetToSet( 'Tyson'     , Families)
-  AddSetToSet( 'Hedgepeth' , Families)
-  AddSetToSet( 'Strickland', Families)
-  AddSetToSet( 'Kosik'     , Families)
-  AddSetToSet( 'Marmaro'   , Families)
-  AddSetToSet( 'Caps'      , Families)
-  AddSetToSet( 'Rankin'    , Families)
+    #ShowPixList(PixList)
+    ShowPixListThumbnails(PixList)
 
-  Friends = NewSet('Friends')
-  Circus = AddSetToSet('Circus', Friends)
+    # Choose a picture (in Circus)
 
-  PixList = NewPixList('*.jpg')
-  if len(PixList) == 0:
-    print('*** No jpg files found in working directory!')
-    os._exit(1)
+    Pix = GetPixByFilename('images/20130713_150852.jpg')
+    if not Pix:
+      print('*** No images/20130713_150852.jpg file found in working directory!')
+      os._exit(1)
 
-  AddPixListToSet(PixList, Circus)
+    #ShowPix(Pix)
 
-  # Choose a picture (in Circus)
+    # Unlink it from all sets (Circus)
 
-  PixList = GetPixListByFilename('20130713_150852.jpg')
-  if len(PixList) == 0:
-    print('*** No 20130713_150852.jpg file found in working directory!')
-    os._exit(1)
+    for Rel in GetRelListOfPix(Pix):
+      Rel.delete()
 
-  # Unlink it from all sets (Circus)
+    # Link it to two new sets
 
-  for Rel in GetRelListOfPix(PixList[0]):
-    Rel.delete()
+    AddPixListToSet([Pix], GetSetByName('Tyson'))
+    AddPixListToSet([Pix], GetSetByName('Nagy' ))
 
-  # Link it to two new sets
+def makeThumbs(imgdir, size=(100, 100), thumbdir='thumbs'):
+  if not os.path.exists(thumbdir):
+    os.mkdir(thumbdir)
+  thumbs = []
+  size = 128, 128
+  for imgfile in glob.glob('%s/*.jpg' % (imgdir)):
+    #print('imgfile = [%s]' % (imgfile))
+    file, ext = os.path.splitext(imgfile)
+    base = file.split('/')[-1]
+    basefile = '%s%s' % (base, ext)
+    thumbpath = 'thumbs/%s.jpg' % (base)
+    if os.path.exists(thumbpath):
+      thumbobj = Image.open(thumbpath)
+      thumbs.append((basefile, thumbobj))
+    else:
+      thumbobj = Image.open(imgfile)
+      thumbobj.thumbnail(size, Image.ANTIALIAS)
+      thumbobj.save(thumbpath, 'JPEG')
+      thumbs.append((basefile, thumbobj))
+  return thumbs
 
-  AddPixListToSet(PixList, GetSetByName('Tyson'))
-  AddPixListToSet(PixList, GetSetByName('Nagy' ))
+class ViewOne(Toplevel):
+  def __init__(self, imgdir, imgfile):
+    Toplevel.__init__(self)
+    self.title(imgfile)
+    imgpath = os.path.join(imgdir, imgfile)
+    imgobj  = ImageTk.PhotoImage(file=imgpath)
+    Label(self, image=imgobj).pack( )
+    #print(imgpath, imgobj.width(), imgobj.height()) # size in pixels
+    self.savephoto = imgobj # keep reference on me
+
+def viewer(imgdir, kind=Toplevel, cols=None):
+  win = kind( )
+  win.title('Viewer')
+  thumbs = makeThumbs(imgdir)
+  if not cols:
+    cols = int(math.ceil(math.sqrt(len(thumbs)))) # fixed or N x N
+  savephotos = []
+  while thumbs:
+    thumbsrow, thumbs = thumbs[:cols], thumbs[cols:]
+    row = Frame(win)
+    row.pack(fill=BOTH)
+    for (imgfile, imgobj) in thumbsrow:
+      photo   = ImageTk.PhotoImage(imgobj)
+      link    = Button(row, image=photo)
+      handler = lambda savefile=imgfile: ViewOne(imgdir, savefile)
+      link.config(command=handler)
+      link.pack(side=LEFT, expand=YES)
+      savephotos.append(photo)
+
+  Button(win, text='Quit', command=win.quit).pack(fill=X)
+  return win, savephotos
+
+def Tk_Test():
+  main, save = viewer('images', kind=Tk)
+  main.mainloop()
+
+#Neo4j_Test()
+Tk_Test()
 
 # -------------------------------------------------------------------------------
 # End.
